c7d9e474a002ac3298c91b2341fcc2681f6e031d
Elvio Barbosa <elviobarbosa@gmail.com>
2023-12-28 08:48:50 -0300

    feat: end of module

--- maps-modules/src/extension.ts
+++ maps-modules/src/extension.ts
@@ -1,45 +1,76 @@
 import * as vscode from 'vscode';
 import * as fs from 'fs';
 import * as path from 'path';
-import { FormatedPrefix, formatPrefix, replacePrefixes } from './utils';
+import { createFiles, formatPrefix } from './utils';
 
-type files = {
-	fileName: string,
-	content: string
-}
+
 export function activate(context: vscode.ExtensionContext) {
 
 	console.log('Congratulations, your extension "maps-modules" is now active!');
 
 	let disposable = vscode.commands.registerCommand('maps-modules.createMapsStructure', async () => {
-		const workspacePath = vscode.workspace.rootPath;
+		
+		const selectedFolders = await vscode.window.showOpenDialog({
+            canSelectFiles: false,
+            canSelectFolders: true,
+            canSelectMany: false,
+            openLabel: 'Select Folder'
+        });
+
+		if (!selectedFolders || selectedFolders.length === 0) {
+            return;
+        }
+
+		const workspacePath = selectedFolders[0].fsPath;
 
 		const prefix = await vscode.window.showInputBox({
             prompt: 'Qual o nome do módulo?'
         });
 
         const folderStructure = [
+			'/',
 			'components', 
 			'containers', 
 			'services', 
 			'services/entities', 
 			'services/repository',
-			'services/usecases'
+			'services/usecases',
+			'services/mapper'
 		];
 
         folderStructure.forEach(folder => {
             const folderPathToCreate = path.join(workspacePath || '', folder);
             
 			if (!fs.existsSync(folderPathToCreate)) {
-				fs.mkdirSync(folderPathToCreate, { recursive: true });
+				if (folder !== '/') {
+					fs.mkdirSync(folderPathToCreate, { recursive: true });
+				}
+
+				if (folder === '/') {
+					createFiles(
+						'', 
+						workspacePath || '',
+						[
+							{
+								fileName: 'module.ts',
+								content: 'module.txt'
+							},
+							{
+								fileName: 'routing.module.ts',
+								content: 'routing.module.txt'
+							},
+						],
+						formatPrefix(prefix)
+					);
+				}		
 
 				if (folder === 'services/entities') {
-					createUsecaseFiles(
+					createFiles(
 						folderPathToCreate, 
 						workspacePath || '',
 						[
 							{
-								fileName: 'active.entity.ts',
+								fileName: 'geral.entity.ts',
 								content: 'entities/request-response.txt'
 							},
 						],
@@ -47,8 +78,22 @@ export function activate(context: vscode.ExtensionContext) {
 					);
 				}
 
+				if (folder === 'services/repository') {
+					createFiles(
+						folderPathToCreate, 
+						workspacePath || '',
+						[
+							{
+								fileName: 'repository.ts',
+								content: 'repository/repository.txt'
+							},
+						],
+						formatPrefix(prefix)
+					);
+				}
+
 				if (folder === 'services/usecases') {
-					createUsecaseFiles(
+					createFiles(
 						folderPathToCreate, 
 						workspacePath || '',
 						[
@@ -89,26 +134,4 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(disposable);
 }
 
-function createUsecaseFiles(folderPath: string, workspacePath: string, fileName: files[], prefix: FormatedPrefix) {
-    
-    fileName.forEach(file => {
-		//path.join(__dirname, 'templates', 'active.txt')
-        const filePath = path.join(folderPath, `${prefix.kebabCase}-${file.fileName}`);
-	    
-        // Ler o conteúdo de um arquivo existente (por exemplo, 'template.ts')
-        //const templateFilePath = path.join(workspacePath, file.content);
-		const templateFilePath = path.join(__dirname, 'templates', file.content);
-		//const codeAsString = getCodeAsString(filePath);
 
-        const existingContent = fs.readFileSync(templateFilePath, 'utf-8');
-
-        // Incorporar o conteúdo existente no novo arquivo
-        const fileContent = `${replacePrefixes(existingContent, prefix.camelCase, prefix.kebabCase)}`;
-
-        fs.writeFileSync(filePath, fileContent);
-       return path.join(workspacePath, file.content);
-		 
-    });
-}
-
-export function deactivate() {}
--- maps-modules/src/utils.ts
+++ maps-modules/src/utils.ts
@@ -1,3 +1,11 @@
+import * as path from 'path';
+import * as fs from 'fs';
+
+type files = {
+	fileName: string,
+	content: string
+}
+
 export type FormatedPrefix = {
     camelCase: string; 
     kebabCase: string
@@ -24,6 +32,20 @@ export function replacePrefixes(content: string, prefixCamelCase: string, prefix
     return content
         .replace(/\[PREFIX-CAMELCASE\]/g, prefixCamelCase)
         .replace(/\[PREFIX-KABEBCASE\]/g, prefixKebabCase);
+}
+
+export function createFiles(folderPath: string, workspacePath: string, fileName: files[], prefix: FormatedPrefix) {
+    
+    fileName.forEach(file => {
+        const filePath = path.join(folderPath, `${prefix.kebabCase}-${file.fileName}`);
+		const templateFilePath = path.join(__dirname, 'templates', file.content);
+        const existingContent = fs.readFileSync(templateFilePath, 'utf-8');
+        const fileContent = `${replacePrefixes(existingContent, prefix.camelCase, prefix.kebabCase)}`;
+
+        fs.writeFileSync(filePath, fileContent);
+        return path.join(workspacePath, file.content);
+		 
+    });
 }
 
 // function getCodeAsString(filePath: string): string | null {
